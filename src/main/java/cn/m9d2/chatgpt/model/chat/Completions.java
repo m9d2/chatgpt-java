469cd5a3be6fd866b767709d88af58120d81f08b
gaoy <158612783@qq.com>
2023-03-24 21:51:49 +0800

    支持gtp4

--- src/main/java/cn/m9d2/chatgpt/model/chat/Completions.java
+++ src/main/java/cn/m9d2/chatgpt/model/chat/Completions.java
@@ -47,7 +47,11 @@ public class Completions implements Serializable {
     @AllArgsConstructor
     public enum Model {
         GPT_3_5_TURBO("gpt-3.5-turbo"),
-        GPT_3_5_TURBO_0301("gpt-3.5-turbo");
+        GPT_3_5_TURBO_0301("gpt-3.5-turbo"),
+        GPT_4("gpt-4"),
+
+        GPT_4_32K("gpt-4-32k"),
+        ;
         private final String value;
 
     }
